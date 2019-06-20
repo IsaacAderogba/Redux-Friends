e01cde5c7eda4be61f26483b7c62c7eb2bb2c494
Isaac Aderogba <isaacaderogba1@gmail.com>
2019-06-20 13:37:23 +0100

    Scaffold out login page with forms and button

--- friends/src/pages/Login.js
+++ friends/src/pages/Login.js
@@ -1,9 +1,28 @@
-import React from 'react';
+import React from "react";
 
 const Home = () => {
-    return (
-        <div>Login</div>
-    )
-}
+  let userRef = React.createRef();
+  let passRef = React.createRef();
+
+  const onLogin = () => {
+    const username = userRef.current.value;
+    const password = passRef.current.value;
+    console.log(username, password)
+    // call login action
+  };
+
+  return (
+    <div>
+      <h3>Login</h3>
+      <div>
+        username <input type="text" ref={userRef} />
+      </div>
+      <div>
+        password <input type="text" ref={passRef} />
+      </div>
+      <button onClick={onLogin}>Log in</button>
+    </div>
+  );
+};
 
 export default Home;
