b41552d459b289b4195f76463907c58dc781def1
Isaac Aderogba <isaacaderogba1@gmail.com>
2019-06-20 12:33:42 +0100

    Create action types

---
+++ friends/src/actions/index.js
@@ -0,0 +1,6 @@
+export const LOGIN = "LOGIN";
+export const GET_FRIENDS = "GET_FRIENDS";
+export const GET_FRIEND = "GET_FRIEND";
+export const POST_FRIEND = "POST_FRIEND";
+export const PUT_FRIEND = "PUT_FRIEND";
+export const DELETE_FRIEND = "DELETE_FRIEND";
