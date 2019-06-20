e28b38930c42f8a50b5a0fe6ff98d2b19f167f0c
Isaac Aderogba <isaacaderogba1@gmail.com>
2019-06-20 13:43:45 +0100

    Create login action creator

--- friends/src/actions/index.js
+++ friends/src/actions/index.js
@@ -1,3 +1,5 @@
+import axios from 'axios';
+
 export const LOGIN = "LOGIN";
 export const IS_FETCHING = "IS_FETCHING";
 export const FAILED_FETCH = "FAILED_FETCH"
@@ -6,3 +8,20 @@ export const GET_FRIEND = "GET_FRIEND";
 export const POST_FRIEND = "POST_FRIEND";
 export const PUT_FRIEND = "PUT_FRIEND";
 export const DELETE_FRIEND = "DELETE_FRIEND";
+
+const endpoint = 'http://localhost:5000/api';
+// Login Action Creator
+export const login = (username, password) => dispatch => {
+    const credentials = { username, password }
+    axios.post(`${endpoint}/login`, credentials)
+      .then(res => {
+          console.log(res);
+        //   dispatch({ type: LOGIN, payload: })
+      })
+      .catch(err => {
+          console.log(err)
+      })
+      .finally(() => {
+
+      });
+}
